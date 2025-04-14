de8056746b9cbd806eaa5cbe9e7bf8d751827735
Antti Ollikkala <antti@neksim.com>
2025-04-15 02:11:30 +0300

    8.23

--- library-backend.js
+++ library-backend.js
@@ -16,14 +16,27 @@ mongoose.connect(MONGODB_URI)
 
 
 const {ApolloServer} = require('@apollo/server')
+const { expressMiddleware } = require('@apollo/server/express4')
+const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
+const express = require('express')
+const cors = require('cors')
+const http = require('http')
+
+const { WebSocketServer } = require('ws')
+const { useServer } = require('graphql-ws/use/ws')
+
+const { PubSub } = require('graphql-subscriptions')
+
 const {GraphQLError} = require('graphql')
-const {startStandaloneServer} = require('@apollo/server/standalone')
 
 const jwt = require('jsonwebtoken')
 
 const Book = require('./models/book')
 const Author = require('./models/author')
 const User = require('./models/user')
+
+const pubsub = new PubSub()
 
 const typeDefs = `
   type User {
@@ -80,8 +93,11 @@ const typeDefs = `
     login(
       username: String!
       password: String!
-    ): Token
-    
+    ): Token 
+  }
+  
+  type Subscription {
+    bookAdded: Book!
   }
 `
 
@@ -141,7 +157,7 @@ const resolvers = {
             const book = new Book({
                 title,
                 published,
-                author,
+                author: author._id,
                 genres
             })
 
@@ -156,6 +172,9 @@ const resolvers = {
                     }
                 })
             }
+
+            pubsub.publish('BOOK_ADDED', {bookAdded: book})
+
             return book
         },
         editAuthor: async (_, {name, setBornTo}, {currentUser}) => {
@@ -198,6 +217,11 @@ const resolvers = {
             return {value: jwt.sign({username, id}, process.env.JWT_SECRET)}
         },
     },
+    Subscription: {
+        bookAdded: {
+            subscribe: () => pubsub.asyncIterableIterator('BOOK_ADDED'),
+        },
+    },
     Book: {
         author: async ({author}) => Author.findById(author),
     },
@@ -211,20 +235,57 @@ const server = new ApolloServer({
     resolvers,
 })
 
-startStandaloneServer(server, {
-    listen: {port: 4000},
-    context: async ({req, res}) => {
-        const auth = req ? req.headers.authorization : null
-        if (auth && auth.startsWith('Bearer ')) {
-            const decodedToken = jwt.verify(
-                auth.substring(7), process.env.JWT_SECRET
-            )
-            const currentUser = await User
-                .findById(decodedToken.id)
+const start = async () => {
+    const app = express()
+    const httpServer = http.createServer(app)
 
-            return {currentUser}
-        }
-    },
-}).then(({url}) => {
-    console.log(`Server ready at ${url}`)
-})
+    const wsServer = new WebSocketServer({
+        server: httpServer,
+        path: '/',
+    })
+
+    const schema = makeExecutableSchema({ typeDefs, resolvers })
+    const serverCleanup = useServer({ schema }, wsServer)
+
+
+    const server = new ApolloServer({
+        schema: makeExecutableSchema({ typeDefs, resolvers }),
+        plugins: [
+            ApolloServerPluginDrainHttpServer({ httpServer }),
+            {
+                async serverWillStart() {
+                    return {
+                        async drainServer() {
+                            await serverCleanup.dispose();
+                        },
+                    };
+                },
+            },
+        ],
+    })
+    await server.start()
+    app.use(
+        '/',
+        cors(),
+        express.json(),
+        expressMiddleware(server, {
+            context: async ({req, res}) => {
+                const auth = req ? req.headers.authorization : null
+                if (auth && auth.startsWith('Bearer ')) {
+                    const decodedToken = jwt.verify(
+                        auth.substring(7), process.env.JWT_SECRET
+                    )
+                    const currentUser = await User
+                        .findById(decodedToken.id)
+
+                    return {currentUser}
+                }
+            }
+        }),
+    )
+    const PORT = 4000
+    httpServer.listen(PORT, () =>
+        console.log(`Server is now running on http://localhost:${PORT}`)
+    )
+}
+start()
